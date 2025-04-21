cd44ac56bd466aebae5495e33c64d82e2c58d9df
AnLVcore2709 <anlvcore2709@gmail.com>
2025-04-21 21:40:19 +0700

    Update test_unit_test.py

--- test_unit_test.py
+++ test_unit_test.py
@@ -1,31 +1,31 @@
 import unittest
 from library_item import LibraryItem
 
+
 # ===== Test for LibraryItem =====
 class TestLibraryItem(unittest.TestCase):
     def test_track(self):
-        track = LibraryItem(1, "Song A", "Artist X", 4)
-        self.assertEqual(track.track_id, 1)
-        self.assertEqual(track.get_name(), "Song A")
-        self.assertEqual(track.get_artist(), "Artist X")
-        self.assertEqual(track.get_rating(), 4)
+        track = LibraryItem(3, "Highway to Hell", "AC/DC", 2)
+        self.assertEqual(track.track_id, 3)
+        self.assertEqual(track.get_name(), "Highway to Hell")
+        self.assertEqual(track.get_artist(), "AC/DC")
+        self.assertEqual(track.get_rating(), 2)
         self.assertEqual(track.get_play_count(), 0)
 
     def test_increment_play_count(self):
-        track = LibraryItem(2, "Song B", "Artist Y", 5)
+        track = LibraryItem(2, "Stayin' Alive", "Bee Gees", 5)
         track.increment_play_count()
         track.increment_play_count()
         self.assertEqual(track.get_play_count(), 2)
 
     def test_reset_play_count(self):
-        track = LibraryItem(3, "Song C", "Artist Z", 3)
+        track = LibraryItem(2, "Stayin' Alive", "Bee Gees", 5)
         track.increment_play_count()
         track.increment_play_count()
         track.reset_play_count()
         self.assertEqual(track.get_play_count(), 0)
 
     def test_set_rating(self):
-        track = LibraryItem(4, "Song D", "Artist W", 2)
+        track = LibraryItem(4, "Shape of You", "Ed Sheeran", 1)
         track.set_rating(5)
         self.assertEqual(track.get_rating(), 5)
-
